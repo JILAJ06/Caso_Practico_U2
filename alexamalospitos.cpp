3974bdaa38334c1d869abf1d61f6309336da3dcc
JILAJ06 <jilaj.alexander@gmail.com>
2025-04-04 17:15:37 -0600

    Update alexamalospitos.cpp

--- alexamalospitos.cpp
+++ alexamalospitos.cpp
@@ -1,5 +1,8 @@
-askadfhsadhfsad ñkh
-aslidhald
-.asndkajnd
-{lajsbdkja}}
-amsjbdkajsdkluhad
+#include <iostream>
+usinfg namespace std;
+
+main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+// This is a simple C++ program that prints "Hello, World!" to the console.
