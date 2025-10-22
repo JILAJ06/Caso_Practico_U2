9432d2015c3e0f924e930d6743c7699629c09547
JILAJ06 <jilaj.alexander@gmail.com>
2025-10-22 09:10:52 -0600

    Eliminacion

--- alexamalospitos.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-usinfg namespace std;
-
-main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
-// This is a simple C++ program that prints "Hello, World!" to the console.
